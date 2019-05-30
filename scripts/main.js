83e375df268b755eb009e9eb226f59a52759c8c5
mihael <you@example.com>
2019-05-30 14:08:25 +0200

    masonry, better header

--- scripts/main.js
+++ scripts/main.js
@@ -23,7 +23,17 @@ $(document).ready(function() {
 
       });
 
-    
-
-
 });
+
+
+    // MASONRY
+    $('.grid').masonry({
+        //options
+        itemSelector: '.grid-item',
+        columnWidth: 120,
+        fitWidth: true,
+        gutter: 0
+    });
+
+
+
